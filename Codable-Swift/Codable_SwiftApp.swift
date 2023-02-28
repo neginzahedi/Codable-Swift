244f9c2e1dd497cf0e306698d879a498da2538d9
Negin Zahedi <zahedi.negin@gmail.com>
2023-02-28 17:58:47 -0500

    Initial Commit

---
+++ Codable-Swift/Codable_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codable_SwiftApp.swift
+//  Codable-Swift
+//
+//  Created by Negin Zahedi on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Codable_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
