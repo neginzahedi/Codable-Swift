451d8e7e374509b98df579b3a4b3cd12f83de9fc
Negin Zahedi <zahedi.negin@gmail.com>
2023-03-05 22:47:56 -0500

    implementation of an ImagePicker struct added

---
+++ Codable-Swift/View/ImagePicker.swift
@@ -0,0 +1,46 @@
+//
+//  ImagePicker.swift
+//  Codable-Swift
+//
+//  Created by Negin Zahedi on 2023-02-28.
+//
+/**
+ This file contains the implementation of an ImagePicker struct, which is used to allow the user to select an image from their device's photo library.
+ */
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage?
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = false
+        picker.sourceType = sourceType
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = selectedImage
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+}
